3ed5193051d04be34fc316676441f8a9e822dae9
Nicolas Martienne <nicolas.martienne@gmail.com>
2022-06-07 16:29:58 +0200

    Add route to delete trick if refused

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use App\Repository\TrickRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,18 +21,26 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/valid/{id}', name: 'app_admin_trick_valid', methods: ['GET'])]
-    public function validTrick(EntityManagerInterface $entityManager, TrickRepository $trickRepository, int $id): Response
+    #[Route('/admin/accept/{id}', name: 'app_admin_trick_accept', methods: ['GET'])]
+    public function acceptTrick(EntityManagerInterface $entityManager, TrickRepository $trickRepository, int $id): Response
     {
         $trick = $trickRepository->findOneBy(['id' => $id]);
         if ($trick) {
             $trick->setValid(true);
             $entityManager->flush();
         }
+        $this->addFlash('success', 'Trick validée !');
         return $this->redirectToRoute('app_admin_trick_index');
     }
-//        return $this->render('admin/admin.html.twig', [
-//            'tricks' => $trickRepository->findAll(),
-//        ]);
-//    }
+
+    #[Route('/{id}', name: 'app_admin_trick_refuse', methods: ['POST'])]
+    public function refuseTrick(Request $request, Trick $trick, TrickRepository $trickRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $trick->getId(), $request->request->get('_token'))) {
+            $trickRepository->remove($trick, true);
+        }
+        $this->addFlash('success', 'Trick supprimée !');
+
+        return $this->redirectToRoute('app_admin_trick_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
